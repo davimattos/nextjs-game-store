172a8651a514616469a3ec646258094e55c6b436
davimattos <davimattosdev@gmail.com>
2021-01-25 16:54:55 -0300

    remove deprecated knobs to use controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,9 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
-}
+  component: Main
+} as Meta
 
-export const Basic = () => (
-  <Main
-    title={text('Title', 'React Boilerplate')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components com StoryBook'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />

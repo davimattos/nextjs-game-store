e4e409ee926f6a1bf725b752d74a2ea90a40ee32
davimattos <davimattosdev@gmail.com>
2021-01-27 10:58:29 -0300

    ajustando media-query e aplicando testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,12 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Game Store/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Game Store/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Game Store/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Game Store/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Game Store/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })

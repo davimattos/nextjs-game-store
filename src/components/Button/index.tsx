fcb2a84d873d523b662cbf8a567557747b8aabeb
davimattos <davimattosdev@gmail.com>
2021-01-30 11:08:00 -0300

    aplicando demais props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,16 +4,18 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
-  icon?: React.ReactNode
+  icon?: JSX.Element
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
   children,
   size = 'medium',
   fullWidth,
-  icon
+  icon,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
